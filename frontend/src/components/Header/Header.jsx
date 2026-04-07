9d63e42ff57a134fa70d178154ad6473416a371b
Spidy-47 <aniketkolhal28@gmail.com>
2026-04-07 13:20:31 +0530

    add header and explore menu

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order Your Favourite Food Here !</h2>
+        <p>Choose from a diverse menu featuring a delectable array of dishes crafted with finest ingredients and satisfy your cravings and elavate your dining experience, one delicious meat at a time.</p>  
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
