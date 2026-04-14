276c96532d8a645ecd376e6e1620a94795851eaf
Spidy-47 <aniketkolhal28@gmail.com>
2026-04-14 17:25:31 +0530

    create structure of login form and add css

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
-import './LoginPopup.css'
-const LoginPopup = () => {
+import React, { useState } from "react";
+import "./LoginPopup.css";
+import { assets } from "../../assets/assets";
+const LoginPopup = ({ setShowLogin }) => {
+  const [currState, setCurrState] = useState("Login");
   return (
-    <div className='login-popup'>
-      login
-    </div>
-  )
-}
+    <div className="login-popup">
+      <form className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>{currState}</h2>
+          <img
+            onClick={() => setShowLogin(false)}
+            src={assets.cross_icon}
+            alt=""
+          />
+        </div>
+        <div className="login-popup-inputs">
+          {currState === "Login" ? (
+            <></>
+          ) : (
+            <input type="text" placeholder="Enter Your Name" required />
+          )}
 
-export default LoginPopup
+          <input
+            type="email"
+            placeholder="Enter Your Name"
+            autoComplete="username"
+            required
+          />
+          <input
+            type="password"
+            placeholder="Enter Password"
+            autoComplete="current-password"
+            required
+          />
+        </div>
+        <button>{currState === "Sign Up" ? "Create Account" : "Login"}</button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>By continuning, I agree to the terms of use & privacy policy.</p>
+        </div>
+        {currState === "Login" ? (
+          <p>
+            Create a new account?{" "}
+            <span onClick={() => setCurrState("Sign Up")}>Click Here</span>
+          </p>
+        ) : (
+          <p>
+            Already have an account?{" "}
+            <span onClick={() => setCurrState("Login")}>Login Here</span>
+          </p>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default LoginPopup;
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
-const Navbar = (setShowLogin) => {
+const Navbar = ({setShowLogin}) => {
 
     const [menu, setMenu] = useState("Home")
   return (
@@ -21,7 +21,7 @@ const Navbar = (setShowLogin) => {
             <img src={assets.basket_icon} alt="" />
             <div className="dot"></div>
         </div>
-            <button onClick={()=>setShowLogin(true)}>Sign In</button>
+            <button onClick={() => setShowLogin(true)}>Sign In</button>
        </div>
     </div>
   )
