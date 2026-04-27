ef9376a1f7e7b4852d04f2f0e6b29963bb919a14
Spidy-47 <aniketkolhal28@gmail.com>
2026-04-27 14:25:44 +0530

    using useState insted of useContext to pass data

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../assets/assets'
 import { Link } from 'react-router-dom'
@@ -7,7 +7,8 @@ const Navbar = ({setShowLogin}) => {
 
     const [menu, setMenu] = useState("Home")
 
-    const {getTotalCartAmount} = useState(StoreContext);
+    const {getTotalCartAmount} = useContext(StoreContext);
+    console.log("TYPE:", typeof getTotalCartAmount);
   return (
     <div className="navbar">
        <Link to='/'><img src={assets.logo} alt="Logo" className="logo" /></Link>
