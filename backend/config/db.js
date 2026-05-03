4455d044872f2283f82c3b0cad9d550c68d32dc0
Spidy-47 <aniketkolhal28@gmail.com>
2026-05-04 01:16:04 +0530

    Removed hardcoded Mongo URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://aniketkolhal28_db_user:751785@cluster0.1utczf0.mongodb.net/Food_Delivery_Web_App_MERN').then(()=>console.log(`DB Connected`));
+    await mongoose.connect(process.env.MONGO_URI).then(()=>console.log(`DB Connected`));
 }
