c2ca4aadc91ce76907e8df497a129880b19e1ce7
Spidy-47 <aniketkolhal28@gmail.com>
2026-05-04 01:58:33 +0530

    chahes uri

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,32 @@
-import express from "express"
-import cors from "cors"
-import { connectDB } from "./config/db.js"
-import foodRouter from "./routes/foodRoute.js"
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-
 //app config
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT || 5000;
 
 //middleware
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 //db connection
 connectDB();
 
 //api endpoints
-app.use("/api/food",foodRouter)
+app.use("/api/food", foodRouter);
 
-app.get("/",(req,res)=>{
-    res.send("API Working")
-})
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
 
-app.listen(port,() =>{
-    console.log(`Server Started on http://localhost:${port}`);
-    
-})
-
+app.listen(port, () => {
+  console.log("PORT VALUE:", port);
+  console.log(`Server Started on http://localhost:${port}`);
+});
